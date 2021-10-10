3f687b702da42849fbebcb2919f3b96c048d919c
rajveerjaiswal997 <88244284+rajveerjaiswal997@users.noreply.github.com>
2021-10-10 17:13:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+menu_list_array = ["Veg Margherita Pizza","veg extravaganza pizza","deluxe veggie pizza","paneer tika pizza","chicken tandoori pizza"
+                    ];
+
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+        // use the sort function as - menu_list_array.sort();
+        htmldata= menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
